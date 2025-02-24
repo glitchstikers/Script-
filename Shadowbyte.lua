eedf030ab1fb265de65439ce08a3c4814486d609
glitchstikers <glitchstikers@gmail.com>
2025-02-25 00:32:05 +0800

    Update Shadowbyte.lua

--- Shadowbyte.lua
+++ Shadowbyte.lua
@@ -1,11 +1,11 @@
-local CoreURL = "https://raw.githubusercontent.com/glitchstikers/Script-/refs/heads/main/Core.lua" 
+local CoreURL = "https://raw.githubusercontent.com/glitchstikers/Script-/refs/heads/main/Core.lua"
 
 local success, CoreScript = pcall(function()
     return loadstring(game:HttpGet(CoreURL))()
 end)
 
 if success and CoreScript then
-    CoreScript:Init()
+    CoreScript:LoadGameScript()
     print("[ShadowByte] Successfully Loaded Core!")
 else
     warn("[ShadowByte] Core Load Failed")
