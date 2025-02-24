a2978d41e7be7362df5eaf9e6d096619a3e669e7
glitchstikers <glitchstikers@gmail.com>
2025-02-25 00:02:20 +0800

    Create Shadowbyte.lua

---
+++ Shadowbyte.lua
@@ -0,0 +1,12 @@
+local CoreURL = "https://yourdomain.com/core.lua" 
+
+local success, CoreScript = pcall(function()
+    return loadstring(game:HttpGet(CoreURL))()
+end)
+
+if success and CoreScript then
+    CoreScript:Init()
+    print("[ShadowByte] Successfully Loaded Core!")
+else
+    warn("[ShadowByte] Core Load Failed")
+end
