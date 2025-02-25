65ec97d6247764ee49d6d7df882171c61cd42fe4
glitchstikers <glitchstikers@gmail.com>
2025-02-25 12:30:50 +0800

    Update GameList.lua

--- GameList.lua
+++ GameList.lua
@@ -1,5 +1,5 @@
 local Games = {
-  [2753915549] = "https://raw.githubusercontent.com/AhmadV99/Script-Games/main/Blox%20Fruit.lua", Test
+  [4483381587] = "https://raw.githubusercontent.com/glitchstikers/Script-/refs/heads/main/Game%20Script/a%20literal%20baseplate.lua", -- a literal baseplate
 }
 
 return Games
