6085e7a82e137b3134eb6c9656fdbae315a2d7a2
glitchstikers <glitchstikers@gmail.com>
2025-02-25 12:27:11 +0800

    Update GameList.lua

--- GameList.lua
+++ GameList.lua
@@ -1,24 +1,5 @@
-local Core = {}
-
-Core.GameList = {
-    [4483381587] = "https://raw.githubusercontent.com/glitchstikers/Script-/refs/heads/main/GameList/TestGame.lua" -- Replace with actual game IDs and script URLs
+local Games = {
+  [2753915549] = "https://raw.githubusercontent.com/AhmadV99/Script-Games/main/Blox%20Fruit.lua", Test
 }
 
-function Core:LoadGameScript()
-    local placeId = game.PlaceId
-    local scriptURL = self.GameList[placeId]
-
-    if scriptURL then
-        local success, scriptFunc = pcall(loadstring(game:HttpGet(scriptURL)))
-        if success and scriptFunc then
-            scriptFunc()
-            print("[Core] Successfully Loaded Script for Game ID: " .. placeId)
-        else
-            warn("[Core] Failed to Load Script for Game ID: " .. placeId)
-        end
-    else
-        warn("[Core] No Supported Script for this Game.")
-    end
-end
-
-return Core
+return Games
